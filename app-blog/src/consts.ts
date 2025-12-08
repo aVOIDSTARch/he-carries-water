6b39952d7c266bc81c50f0320d2fdd72d8f93553
louisc <lcasine@my.wgu.edu>
2025-12-08 13:43:50 -0500

    First few personalizations

--- app-blog/src/consts.ts
+++ app-blog/src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'He Carries Water';
+export const SITE_DESCRIPTION = "Louis Casinelli's Public Interactive Mindcaster";
