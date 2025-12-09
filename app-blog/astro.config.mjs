1b840c0ec316d1244373ba5f4b749d74d79021ff
louisc <lcasine@my.wgu.edu>
2025-12-09 10:41:24 -0500

    fix: tailwindcss postcss error

--- app-blog/astro.config.mjs
+++ app-blog/astro.config.mjs
@@ -10,8 +10,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
-  integrations: [mdx(), sitemap(), preact()],
+  site: "https://hecarrieswater.com",
+  integrations: [mdx(), sitemap(), preact({ devtools: true })],
 
   vite: {
     plugins: [tailwindcss()],
