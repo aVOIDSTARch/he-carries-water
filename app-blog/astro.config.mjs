e9593c9115a29b5f0b27837afc2683016df23264
Louis Casinelli Jr <103291324+aVOIDSTARch@users.noreply.github.com>
2025-12-08 19:10:09 -0500

    added more package configuation

--- app-blog/astro.config.mjs
+++ app-blog/astro.config.mjs
@@ -1,16 +1,16 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
-import preact from '@astrojs/preact';
+import preact from "@astrojs/preact";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: "https://example.com",
   integrations: [mdx(), sitemap(), preact()],
 
   vite: {
